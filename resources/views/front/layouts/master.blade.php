2a96b7ee8cdfcad778370fc9c63156b6ed0e4145
surcodev <temporal98x@gmail.com>
2025-10-20 16:02:29 -0500

    update

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -8,6 +8,13 @@
 
     <link rel="icon" type="image/png" href="{{ asset('uploads/'.$global_setting->favicon) }}">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap" rel="stylesheet">
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+  integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
 
     @include('front.layouts.style')
     @include('front.layouts.script')
